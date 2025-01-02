cdeaa8ddfc9aba9745a907cf497c43d0f2fae33c
Lore Sanabria <154547100+Lore-code-dev@users.noreply.github.com>
2025-01-02 09:46:00 -0300

    cambios
    cambios nuevos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,14 +13,15 @@ const Hero = () => {
                 <span className='text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-blue-500'>
                     Lorena Sanabria
                 </span>
-                , Frontend and Backend Developer
+                , Fullstack Developer
             </h1>
             <p className='mt-4 text-lg text-gray-300'>
-                I specialize in building modern and responsive web applications.
+                I specialize in building modern and responsive web applications,
+                with expertise in managing databases and developing mobile
+                applications.
             </p>
             <div className='mt-8 space-x-4'>
                 <button
-                
                     className='bg-gradient-to-r from-blue-900 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
                     Contact With Me
@@ -28,7 +29,7 @@ const Hero = () => {
                 <button
                     className='bg-gradient-to-r from-pink-800 to-violet-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
-                    Resume
+                    Download CV
                 </button>
             </div>
         </div>
